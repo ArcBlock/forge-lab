397e83c394f038fecd04c4e70a82a1ac272b3963
Peiling Ding <dingpl716@gmail.com>
2019-12-09 14:49:33 -0800

    move protocols

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule ForgeLab.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:forge_sdk, "~> 1.0.2"}
     ]
   end
 end
